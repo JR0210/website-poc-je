b99788eb7cebf99c149b81e4c54998e3d4ed4cc6
Jacob Robinson <robinson.jacob.e@gmail.com>
2022-03-03 17:33:44 +0000

    added sidebar content, changed colours, fixed css issues

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -10,14 +10,25 @@ export default function Sidebar() {
 
   return (
     <>
-      <div onClick={handlemenuOpenClick} className={`hidden md:flex fixed text-base transition-right duration-500 ease-in-out font-sans top-64 transform rotate-90 px-5 py-3 ${menuOpen ? `right-1/3` : `right-0`}  rounded-b-md shadow-lg bg-white cursor-pointer text-blue-600 flex flex-row space-x-2 justify-center items-center z-10`}>
-        <span>Self Service</span>
-        <MdKeyboardArrowDown className={`transition-rotate duration-500 ease-out ${menuOpen ? `rotate-180` : `rotate-0`}  h-6 w-6 text-cyan`} />
+      <div onClick={handlemenuOpenClick} className={`hidden md:flex fixed text-base transition-right duration-500 ease-in-out font-sans top-[45vh] origin-top-right transform rotate-90 px-5 py-3 ${menuOpen ? `right-full lg:right-1/2 2xl:right-1/3` : `right-0`}  rounded-b-md shadow-lg bg-blue-200 text-white cursor-pointer flex flex-row space-x-2 justify-center items-center z-10`}>
+        <span className="rotate-180">Self Service</span>
+        <MdKeyboardArrowDown className={`transition-rotate duration-500 ease-out ${menuOpen ? `rotate-180` : `rotate-0`}  h-6 w-6`} />
       </div>
 
-      <div className={`fixed transition-right duration-500 ease-in-out ${menuOpen ? `right-0` : `-right-1/3`} top-0 h-full w-1/3 bg-white shadow-lg p-6 text-blue-600 z-10`}>
+      <div className={`fixed transition-right duration-500 ease-in-out ${menuOpen ? `right-0` : `-right-1/3`} top-0 h-full w-full lg:w-1/2 2xl:w-1/3 bg-blue-300 shadow-lg p-6 z-10`}>
         <div className="relative flex flex-col">
-          <MdOutlineClose onClick={handlemenuOpenClick} className="absolute top-0 right-0 h-8 w-8 text-cyan hover:text-blue-600 cursor-pointer" />
+          <MdOutlineClose onClick={handlemenuOpenClick} className="absolute top-0 right-0 h-8 w-8 text-white cursor-pointer" />
+          <div className="p-16">
+              <span className="text-white font-bold">
+                  Self-Service
+              </span>
+              <div>items</div>
+              
+              <span className="text-white font-bold">
+                  Contact
+              </span>
+              <div>items</div>
+          </div>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0C007D',
+        blue: {
+          '100': '#484EB5',
+          '200': '#0B0868',
+          '300': '#0B0947',
+        }
+      }
+    },
   },
   plugins: [],
 }
